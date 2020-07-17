2774e335716f31acb0e6e6cb6131bce766e0b68e
Felix Chin <felixmchin@gmail.com>
2020-07-17 11:39:44 -0700

    added getAverageGrade method to App component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -6,38 +6,44 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      grades: [],
-      average: 0
+      grades: []
     };
-    this.getAverageGrade = this.getAverageGrade.bind(this);
-  }
-
-  getAverageGrade() {
-    const grades = this.state.grades.slice(0);
-    let sum = null;
-    let average = this.state.average;
-    for (let i = 0; i < grades.length; i++) {
-      sum += grades[i].grade;
-    }
-    if (isNaN(sum / grades.length)) {
-      average = 'N/A';
-    } else {
-      average = Math.floor(sum / grades.length);
-    }
-    return average;
   }
 
   componentDidMount() {
     fetch('/api/grades')
       .then(res => res.json())
-      .then(data => this.setState({ grades: data }))
+      .then(data => {
+        this.setState({ grades: data });
+        this.getAverageGrade();
+      })
       .catch(err => console.error(err));
+  }
+
+  getAverageGrade() {
+    const grades = this.state.grades.slice(0);
+    console.log('Grades:', grades);
+    let sum = null;
+    let average = null;
+    grades.forEach(grade => sum += grade.grade)
+    console.log('Sum:', sum)
+    if (isNaN(sum / grades.length)) {
+      average = 'N/A';
+    } else {
+      average = Math.floor(sum / grades.length);
+    }
+    console.log('Average:', average)
+    return average;
+  }
+
+  componentDidUpdate() {
+    console.log(this.state)
   }
 
   render() {
     return (
       <div>
-        <Header average={this.state.average}/>
+        <Header average={''}/>
         <GradeTable grades={this.state.grades}/>
       </div>
     );
